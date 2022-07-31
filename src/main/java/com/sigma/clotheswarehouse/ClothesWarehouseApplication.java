72cf65ce7d3f99320e07e45fa3fa9ce6c3c0640e
zukhriddin <@zukhriddin808gmail.com>
2022-07-31 23:00:18 +0500

    first

---
+++ src/main/java/com/sigma/clotheswarehouse/ClothesWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.sigma.clotheswarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothesWarehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sigma/clotheswarehouse/ClothesWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigma.clotheswarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
