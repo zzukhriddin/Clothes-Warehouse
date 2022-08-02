dacbe21a77e39dc0f87e89f98e2b0435028d6775
Ramziddin <xolmaxmatovramziddin7@gmail.com>
2022-08-02 15:28:43 +0500

    Post Path was created for new IncomeMaterials and IncomeMaterialMapper

--- src/main/java/com/sigma/clotheswarehouse/controller/IncomeMaterialController.java
+++ src/main/java/com/sigma/clotheswarehouse/controller/IncomeMaterialController.java
@@ -1,9 +1,27 @@
 package com.sigma.clotheswarehouse.controller;
 
+import com.sigma.clotheswarehouse.payload.ApiResponse;
+import com.sigma.clotheswarehouse.payload.IncomeMaterialDTO;
+import com.sigma.clotheswarehouse.service.IncomeMaterialService;
+import com.sigma.clotheswarehouse.utils.AppConstant;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(AppConstant.BASE_PATH + "/incomeMaterial")
 public class IncomeMaterialController {
+
+    private final IncomeMaterialService incomeMaterialService;
+
+    @PostMapping
+    public HttpEntity<?> addIncomeMaterials(@RequestBody List<IncomeMaterialDTO> incomeMaterialDTOList) {
+        ApiResponse apiResponse = incomeMaterialService.addIncomeMaterial(incomeMaterialDTOList);
+        return ResponseEntity.ok(apiResponse);
+    }
+
 }
