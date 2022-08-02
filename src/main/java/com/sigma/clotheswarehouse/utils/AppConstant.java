72fc9302ba79eeeb7bae3b35fd1a3039c0c7a1ff
Ramziddin <xolmaxmatovramziddin7@gmail.com>
2022-08-02 18:17:05 +0500

    DEFAULT PAGE, SIZE and MAX_SIZE

--- src/main/java/com/sigma/clotheswarehouse/utils/AppConstant.java
+++ src/main/java/com/sigma/clotheswarehouse/utils/AppConstant.java
@@ -3,4 +3,11 @@ package com.sigma.clotheswarehouse.utils;
 public interface AppConstant {
 
     String BASE_PATH = "/api/clothes/warehouse";
+
+    String DEFAULT_PAGE = "0";
+
+    String DEFAULT_SIZE = "10";
+
+    Integer MAX_PAGE_SIZE = 20;
+
 }
