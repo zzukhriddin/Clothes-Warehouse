4ed025abc4e6b4e21cc8913f43c375463c4af5ac
samir <sharipovsamir480@gmail.com>
2022-08-02 19:49:28 +0500

    product controller crud done 99.9%

--- src/main/java/com/sigma/clotheswarehouse/mapper/ProductMapper.java
+++ src/main/java/com/sigma/clotheswarehouse/mapper/ProductMapper.java
@@ -2,8 +2,11 @@ package com.sigma.clotheswarehouse.mapper;
 
 import com.sigma.clotheswarehouse.entity.Product;
 import com.sigma.clotheswarehouse.payload.ProductDTO;
+import com.sigma.clotheswarehouse.payload.ProductGetDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
@@ -14,4 +17,9 @@ public interface ProductMapper {
 
     @Mapping(target = "measurementId", source = "measurement.id")
     ProductDTO toDTO(Product product);
+
+    @Mapping(target = "measurementDTO", source = "measurement")
+    ProductGetDto getDTO(Product product);
+
+    List<ProductGetDto> getDTOs(List<Product> productList);
 }
